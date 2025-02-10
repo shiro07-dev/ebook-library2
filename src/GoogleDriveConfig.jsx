af39a45aa2e661cf31f5d6bfee64feb1933f3ebe
irwanr <irwandhanker@gmail.com>
2025-02-10 17:56:49 +0700

    Menghapus FIle Config

--- src/GoogleDriveConfig.jsx
+++
@@ -1,4 +0,0 @@
-export const CLIENT_ID = "524340770826-2dcq7rrp3fshngao6t93rq57ql0smv69.apps.googleusercontent.com";
-export const API_KEY = "AIzaSyBO_oXo9wHm9mjY6X5yjQMUTFeP8n4E7sc";
-export const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-export const SCOPES = "https://www.googleapis.com/auth/drive.file";
